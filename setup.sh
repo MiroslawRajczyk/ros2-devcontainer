49a2d72330821635d4d58eee731b5aafc966c0a5
MiroslawRajczyk <miroslaw.rajczyk@gmail.com>
2024-10-08 20:22:16 +0200

    Pull scorpio repo: external_packages

--- setup.sh
+++ setup.sh
@@ -6,6 +6,7 @@ ROS2_WS_DIR="/root/ros2_ws/src"
 # Clone your repositories
 # Replace these with the actual GitHub URLs for your repositories
 git clone https://github.com/ScorpioOrganization/core.git $ROS2_WS_DIR/core
+git clone https://github.com/ScorpioOrganization/external_packages $ROS2_WS_DIR/external_packages
 git clone https://github.com/RobotWebTools/rosbridge_suite.git $ROS2_WS_DIR/rosbridge_suite
 # Source the ROS 2 Jazzy setup
 source /opt/ros/jazzy/setup.bash
